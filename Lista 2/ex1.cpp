507c0f7d2bf46736969cb562f0480d79c3148bcc
Weslley Caetano <weslleycaetanosoares@gmail.com>
2024-06-14 22:57:21 -0300

    first commit

---
+++ Lista 2/ex1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+int main () {
+  int estadoCivil, icasado, isolteiro, iviuvo, divorciado, qcasadas=0, qsolteiras=0, medViuvas, porcDivorci;
+
+  cout << "Qual é seu estado cívil? (1 - Casado 2 - Solteiro 3 - Viúvo 4 - Divorciado)" << endl;
+  cin >> estadoCivil;
+  cout << "Qual sua idade? "
+  cin >> idade;
+
+  while (idade >= 1) {
+    switch(idade){
+      case 1: 
+      qcasadas++;
+      break;
+
+      case 2:
+      qsolteiras++;
+      break;
+    
+      case 3:
+      iviuvo 
+
+  }
+  cout << "Qual é seu estado cívil? (1 - Casado 2 - Solteiro 3 - Viúvo 4 - Divorciado)" << endl;
+cin >> estadoCivil;
+
+}
+
